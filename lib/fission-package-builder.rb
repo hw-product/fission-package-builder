20157e4dffa22cf753cbd6e9ce6f6c9678e5f619
Chris Roberts <chrisroberts.code@gmail.com>
2013-10-18 11:07:57 -0700

    Fix the validations subdirectory loading

--- lib/fission-package-builder.rb
+++ lib/fission-package-builder.rb
@@ -3,5 +3,5 @@ require 'fission-package-builder/version'
 require 'fission-package-builder/builder'
 
 Dir.glob(File.join(File.dirname(__FILE__), 'fission-package-builder', 'validations', '*.rb')).each do |path|
-  require "fission-package-builder/#{File.basename(path).sub('.rb', '')}"
+  require "fission-package-builder/validations/#{File.basename(path).sub('.rb', '')}"
 end
