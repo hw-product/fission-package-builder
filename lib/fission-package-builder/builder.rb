867ccf82dcdce6437b5afa31181fdb5f7a4e4ec7
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-08 07:45:02 -0800

    Move build off host and into container

--- lib/fission-package-builder/builder.rb
+++ lib/fission-package-builder/builder.rb
@@ -1,5 +1,8 @@
 require 'fission/callback'
 require 'fission-package-builder/packager'
+require 'elecksee/ephemeral'
+
+Lxc.use_sudo = true
 
 module Fission
   module PackageBuilder
@@ -26,43 +29,51 @@ module Fission
         JSON.dump(
           :fission => {
             :build => config.merge(
-              :target_store => params[:repository][:path]
+              :target_store => repository_copy(params[:message_id], params[:repository][:path])
             )
+          },
+          :fpm_tng => {
+            :package_dir => workspace(params[:message_id], :packages)
           },
           :run_list => ['recipe[fission]']
         )
       end
 
       def start_build(uuid, json)
-        json_path = File.join(workspace(:first_runs), "#{uuid}.json")
+        json_path = File.join(workspace(uuid, :first_runs), "#{uuid}.json")
         File.open(json_path, 'w') do |f|
           f.puts json
         end
-        log_file_path = File.join(workspace(:log), "#{uuid}.log")
+        log_file_path = File.join(workspace(uuid, :log), "#{uuid}.log")
         log_file = File.open(log_file_path, 'w')
         log_file.sync = true
         command = [chef_exec_path, '-j', json_path, '-c', write_solo_config(uuid)]
-        debug "Starting command: #{command.join(' ')}"
-        process_manager.process(uuid, command) do |process|
-          process.io.stdout = process.io.stderr = log_file
-          process.cwd = '/tmp'
-          process.detach = true
-          process.start
+        ephemeral = Lxc::Ephemeral.new(
+          :original => 'ubuntu_1204',
+          :bind => '/tmp/packages',
+          :ephemeral_command => command.join(' ')
+        )
+        begin
+          ephemeral.start!
+        rescue Lxc::CommandFailed => e
+          error "Package build failed: #{e.result.stderr}"
         end
-
+        debug "Finished command: #{command.join(' ')}"
       end
 
-      def workspace(thing=nil)
+      def workspace(uuid, thing=nil)
         base = Carnivore::Config.get(:fission, :package_builder, :working_directory) || '/tmp'
-        path = File.join(base, thing.to_s)
+        path = File.join(base, uuid, thing.to_s)
         unless(File.directory?(path))
           FileUtils.mkdir_p(path)
         end
         path
       end
 
-      def solo_config_path(uuid)
-        File.join(workspace(:solos), "#{uuid}-solo.rb")
+      def repository_copy(uuid, repo_path)
+        code_path = workspace(uuid, :code)
+        FileUtils.cp_r("#{repo_path}/.", code_path)
+        code_path
       end
 
       def fission_cookbook_path
@@ -77,15 +88,22 @@ module Fission
       end
 
       def write_solo_config(uuid)
-        solo_path = solo_config_path(uuid)
+        solo_path = File.join(workspace(uuid, :solos), "solo.rb")
         unless(File.exists?(solo_path))
-          cache_path = workspace("chef-cache/#{uuid}")
+          cache_path = workspace(uuid, :chef_cache)
+          cookbook_path = cookbook_copy(uuid)
           File.open(solo_path, 'w') do |file|
             file.puts "file_cache_path '#{cache_path}'"
-            file.puts "cookbook_path '#{fission_cookbook_path}'"
+            file.puts "cookbook_path '#{cookbook_path}'"
           end
         end
         solo_path
+      end
+
+      def cookbook_copy(uuid)
+        local_path = workspace(uuid, :cookbooks)
+        FileUtils.cp_r("#{fission_cookbook_path}/.", local_path)
+        local_path
       end
 
       def chef_exec_path
