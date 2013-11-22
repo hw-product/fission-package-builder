5ae8ae00b4e52ff52f234747433bf76d069faa36
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-22 09:26:33 -0800

    Remove internal validators

--- lib/fission-package-builder.rb
+++ lib/fission-package-builder.rb
@@ -1,7 +1,3 @@
 require 'fission'
 require 'fission-package-builder/version'
 require 'fission-package-builder/builder'
-
-Dir.glob(File.join(File.dirname(__FILE__), 'fission-package-builder', 'validations', '*.rb')).each do |path|
-  require "fission-package-builder/validations/#{File.basename(path).sub('.rb', '')}"
-end
