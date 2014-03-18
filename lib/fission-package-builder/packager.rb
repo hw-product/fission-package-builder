b55aac73ad569a83b9c5362810f93ea979267bfa
Chris Roberts <chrisroberts.code@gmail.com>
2014-03-17 18:41:26 -0700

    Force ruby based packager file evaluation through ephemeral container process

--- lib/fission-package-builder/packager.rb
+++ lib/fission-package-builder/packager.rb
@@ -1,4 +1,5 @@
 require 'attribute_struct'
+require 'multi_json'
 
 module Fission
   module PackageBuilder
@@ -12,18 +13,67 @@ module Fission
         end
 
         def load(path)
-          if(File.exists?(path))
-            if(defined?(Sandbox))
-              Sandbox.eval(File.read(path))
-            else
-              PackageBuilder.class_eval do
-                eval(File.read(path))
-              end
-            end
+          if(File.exist?(path))
+            json_load(path) || ruby_load(path)
           else
             raise Error::PkgrFileNotFound.new("Failed to locate pkgr file: #{path}")
           end
         end
+
+        def wrapper_file
+          file = <<-WRAPPER
+          require 'attribute_struct'
+          require 'json'
+          require 'tempfile'
+          require 'securerandom'
+          class Packager < AttributeStruct
+            class << self
+              def build(&block)
+                file = File.open(File.join('/tmp', SecureRandom.uuid), 'w')
+                file.puts JSON.dump(new(&block)._dump)
+                file.close
+                $stdout.puts file.path
+              end
+            end
+          end
+          WRAPPER
+        end
+
+        def ruby_load(path)
+          ephemeral = Lxc::Ephemeral.new(
+            :original => 'ubuntu_1204',
+            :bind => File.dirname(path)
+          )
+          result = nil
+          begin
+            ephemeral.create!
+            File.open(ephemeral.lxc.rootfs.join('tmp/pkgr.rb'), 'w') do |file|
+              file.puts wrapper_file
+            end
+            com = "/opt/chef/embedded/bin/ruby -r/tmp/pkgr.rb -C#{File.dirname(path)} #{path}"
+            result = ephemeral.lxc.execute(
+              "/opt/chef/embedded/bin/ruby -r/tmp/pkgr -C#{File.dirname(path)} #{path}"
+            )
+            content = File.read(
+              ephemeral.lxc.rootfs.join(
+                result.stdout.strip
+              )
+            )
+            result = MultiJson.load(content, :symbolize_keys => true)
+          ensure
+            ephemeral.cleanup
+          end
+          result
+        end
+
+        def json_load(path)
+          begin
+            MultiJson.load(File.read(path), :symbolize_keys => true)
+          rescue MultiJson::LoadError
+            nil
+          end
+        end
+
       end
     end
 
