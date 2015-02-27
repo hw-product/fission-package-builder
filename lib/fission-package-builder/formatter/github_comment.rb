57d70859833b0be41109377a173c8ae7ba3b2f0e
Chris Roberts <chrisroberts.code@gmail.com>
2015-02-27 15:52:18 -0800

    Update github comment formatter to use github kit

--- lib/fission-package-builder/formatter/github_comment.rb
+++ lib/fission-package-builder/formatter/github_comment.rb
@@ -7,26 +7,34 @@ module Fission
       class GithubComment < Fission::Formatter
 
         SOURCE = :package_builder
-        DESTINATION = :github_comment
+        DESTINATION = :github_kit
 
         def format(payload)
           pkg = payload[:data][:package_builder]
           origin_info = origin(payload[:brand])
           if(payload[:status].to_s == 'error')
-            payload[:data][:github_comment] = {
-              :message => [
-                "[#{origin_info[:application]}] FAILED #{pkg[:name]} build (version: #{pkg[:version]})",
-                "Package building attempt failed!\n\nExtracted error message:\n",
-                "```",
-                "#{extract_chef_stacktrace(payload) || '<unavailable>'}\n",
-                "```",
-                "- #{job_url(payload)}"
-              ].join("\n")
-            }
+            payload.set(
+              :data, :github_kit, :commit_comment, Smash.new(
+                :respository => payload.get(:data, :code_fetcher, :info, :name),
+                :reference => payload.get(:data, :code_fetcher, :info, :commit_sha),
+                :message => [
+                  "[#{origin_info[:application]}] FAILED #{pkg[:name]} build (version: #{pkg[:version]})",
+                  "Package building attempt failed!\n\nExtracted error message:\n",
+                  "```",
+                  "#{extract_chef_stacktrace(payload) || '<unavailable>'}\n",
+                  "```",
+                  "- #{job_url(payload)}"
+                ].join("\n")
+              )
+            )
           else
-            payload[:data][:github_comment] = {
-              :message => "[#{origin_info[:application]}] New #{pkg[:name]} created (version: #{pkg[:version]})\n\n- #{job_url(payload)}"
-            }
+            payload.set(
+              :data, :github_kit, :commit_comment, Smash.new(
+                :respository => payload.get(:data, :code_fetcher, :info, :name),
+                :reference => payload.get(:data, :code_fetcher, :info, :commit_sha),
+                :message => "[#{origin_info[:application]}] New #{pkg[:name]} created (version: #{pkg[:version]})\n\n- #{job_url(payload)}"
+              )
+            )
           end
         end
 
