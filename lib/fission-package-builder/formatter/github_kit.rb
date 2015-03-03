24fafad78547309f959a73dd6dfa6d4ddc14fb75
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-03 15:29:57 -0800

    Check payload prior to format.

--- lib/fission-package-builder/formatter/github_kit.rb
+++ lib/fission-package-builder/formatter/github_kit.rb
@@ -14,37 +14,39 @@ module Fission
         #
         # @param payload [Smash]
         def format(payload)
-          pkg = payload[:data][:package_builder]
-          origin_info = origin(payload[:brand])
-          if(payload[:status].to_s == 'error')
-            payload.set(
-              :data, :github_kit, :commit_comment, Smash.new(
-                :repository => [
-                  payload.get(:data, :code_fetcher, :info, :owner),
-                  payload.get(:data, :code_fetcher, :info, :name)
-                ].join('/'),
-                :reference => payload.get(:data, :code_fetcher, :info, :commit_sha),
-                :message => [
-                  "[#{origin_info[:application]}] FAILED #{pkg[:name]} build (version: #{pkg[:version]})",
-                  "Package building attempt failed!\n\nExtracted error message:\n",
-                  "```",
-                  "#{extract_chef_stacktrace(payload) || '<unavailable>'}\n",
-                  "```",
-                  "- #{job_url(payload)}"
-                ].join("\n")
+          if(payload.get(:data, :package_builder, :name))
+            pkg = payload[:data][:package_builder]
+            origin_info = origin(payload[:brand])
+            if(payload[:status].to_s == 'error')
+              payload.set(
+                :data, :github_kit, :commit_comment, Smash.new(
+                  :repository => [
+                    payload.get(:data, :code_fetcher, :info, :owner),
+                    payload.get(:data, :code_fetcher, :info, :name)
+                  ].join('/'),
+                  :reference => payload.get(:data, :code_fetcher, :info, :commit_sha),
+                  :message => [
+                    "[#{origin_info[:application]}] FAILED #{pkg[:name]} build (version: #{pkg[:version]})",
+                    "Package building attempt failed!\n\nExtracted error message:\n",
+                    "```",
+                    "#{extract_chef_stacktrace(payload) || '<unavailable>'}\n",
+                    "```",
+                    "- #{job_url(payload)}"
+                  ].join("\n")
+                )
               )
-            )
-          else
-            payload.set(
-              :data, :github_kit, :commit_comment, Smash.new(
-                :repository => [
-                  payload.get(:data, :code_fetcher, :info, :owner),
-                  payload.get(:data, :code_fetcher, :info, :name)
-                ].join('/'),
-                :reference => payload.get(:data, :code_fetcher, :info, :commit_sha),
-                :message => "[#{origin_info[:application]}] New #{pkg[:name]} created (version: #{pkg[:version]})\n\n- #{job_url(payload)}"
+            else
+              payload.set(
+                :data, :github_kit, :commit_comment, Smash.new(
+                  :repository => [
+                    payload.get(:data, :code_fetcher, :info, :owner),
+                    payload.get(:data, :code_fetcher, :info, :name)
+                  ].join('/'),
+                  :reference => payload.get(:data, :code_fetcher, :info, :commit_sha),
+                  :message => "[#{origin_info[:application]}] New #{pkg[:name]} created (version: #{pkg[:version]})\n\n- #{job_url(payload)}"
+                )
               )
-            )
+            end
           end
         end
 
