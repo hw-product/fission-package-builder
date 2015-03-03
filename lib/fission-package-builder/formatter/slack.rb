a1016174140d0119d0e11c5a48d034bcc306d2a2
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-03 15:29:28 -0800

    Only format if payload is ready for formatting

--- lib/fission-package-builder/formatter/slack.rb
+++ lib/fission-package-builder/formatter/slack.rb
@@ -10,30 +10,33 @@ module Fission
         DESTINATION = :slack
 
         def format(payload)
-          pkg = payload[:data][:package_builder]
-          origin_info = origin(payload[:brand])
-          if(payload[:status].to_s == 'error')
-            payload.set(
-              :data, :slack, :messages, [
-                Smash.new(
-                  :message => [
-                    "[#{origin_info[:application]}] FAILED #{pkg[:name]} build (version: #{pkg[:version]})",
-                    "Package building attempt failed!\n\nExtracted error message:\n",
-                    "```",
-                    "#{extract_chef_stacktrace(payload) || '<unavailable>'}\n",
-                    "```",
-                    "- #{job_url(payload)}"
-                  ].join("\n"),
-                  :color => 'red'
-                )
-              ]
-            )
-          else
-            payload.set(
-              :data, :slack, :messages, [
-                :message => "[#{origin_info[:application]}] New #{pkg[:name]} created (version: #{pkg[:version]})\n\n- #{job_url(payload)}"
-              ]
-            )
+          if(payload.get(:data, :package_builder, :name))
+            pkg = payload[:data][:package_builder]
+            origin_info = origin(payload[:brand])
+            if(payload[:status].to_s == 'error')
+              payload.set(
+                :data, :slack, :messages, [
+                  Smash.new(
+                    :message => [
+                      "[#{origin_info[:application]}] FAILED #{pkg[:name]} build (version: #{pkg[:version]})",
+                      "Package building attempt failed!\n\nExtracted error message:\n",
+                      "```",
+                      "#{extract_chef_stacktrace(payload) || '<unavailable>'}\n",
+                      "```",
+                      "- #{job_url(payload)}"
+                    ].join("\n"),
+                    :color => 'danger'
+                  )
+                ]
+              )
+            else
+              payload.set(
+                :data, :slack, :messages, [
+                  :message => "[#{origin_info[:application]}] New #{pkg[:name]} created (version: #{pkg[:version]})\n\n- #{job_url(payload)}",
+                  :color => 'good'
+                ]
+              )
+            end
           end
         end
 
