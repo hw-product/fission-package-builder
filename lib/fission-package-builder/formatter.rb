482ae3b3889fdae41e6a68eb5ae2d62937c07f2b
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-03 15:32:02 -0800

    Upload formatter loading

--- lib/fission-package-builder/formatter.rb
+++ lib/fission-package-builder/formatter.rb
@@ -1,5 +1,5 @@
 require 'fission-package-builder/formatter/mail'
 require 'fission-package-builder/formatter/slack'
-require 'fission-package-builder/formatter/github_kit'
-require 'fission-package-builder/formatter/github_status'
+require 'fission-package-builder/formatter/github_commit_comment'
+require 'fission-package-builder/formatter/github_release'
 require 'fission-package-builder/formatter/code_fetcher'
