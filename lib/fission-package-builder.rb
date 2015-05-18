f8bab2d7be72f08e9303265691ac8f51b9a6b94c
Chris Roberts <code@chrisroberts.org>
2015-05-18 11:20:18 -0700

    Add service registration

--- lib/fission-package-builder.rb
+++ lib/fission-package-builder.rb
@@ -2,3 +2,5 @@ require 'fission'
 require 'fission-package-builder/version'
 require 'fission-package-builder/builder'
 require 'fission-package-builder/formatter'
+
+Fission.service(:package_builder)
